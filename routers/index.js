7c9e1300fd70a454f8324840937946822c7596f1
zhaoying-zoe <1130051785@qq.com>
2021-02-10 21:53:53 +0800

    前台分类处理

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,25 @@
 const express = require('express');// 引入express
 const router = express();// 创建实例
+const CategoryModel = require('../models/category');// 引入user注册文档模型
+const ArticleModel = require('../models/article');// 引入user注册文档模型
 
-// 首页
-router.get('/',(req,res)=>{
+// 获取共通数据
+const getCommonData = async () => {
+    // 分类数据
+    const categoriesPromise = CategoryModel.find({}, "category_name");
+    // 排行榜数据
+    const topArticlesPromise = ArticleModel.find({}, "title click").sort({ click: -1 }).limit(10);
+    const categories = await categoriesPromise;
+    const topArticles = await topArticlesPromise;
+    // return 一个对象(所有的共通数据)
+    return {
+        categories,
+        topArticles,
+    }
+}
+
+// 显示首页
+router.get('/', async (req, res) => {
     /*
     // 获取cookie信息进行验证
     let userInfo = {};// 存cookie信息的空对象
@@ -10,22 +27,36 @@ router.get('/',(req,res)=>{
         userInfo = JSON.parse(req.cookies.get('userInfo'));// 赋值 对象
     }
     */
-    res.render('main/index',{
-        userInfo:req.userInfo,// 把保存的信息携带到前台
+    // 结构赋值接收返回的对象数据
+    const { categories, topArticles, } = await getCommonData();
+    res.render('main/index', {
+        userInfo: req.userInfo,// 把保存的信息携带到前台
+        categories,
+        topArticles,
     });
 })
-// 详情页
-router.get('/detail',(req,res)=>{
-    res.render('main/detail',{
-        userInfo:req.userInfo,
+
+
+// 显示列表页
+router.get('/list/:id', async (req, res) => {
+    const id = req.params;//id是一个对象 id = {id:6023d2376be6fc4668f06ec1}
+    const { categories, topArticles, } = await getCommonData();
+    res.render('main/list', {
+        userInfo: req.userInfo,// 把保存的信息携带到前台
+        categories,
+        currentCategory: id,
+        topArticles,
     });
 })
-// 列表页
-router.get('/list',(req,res)=>{
-    res.render('main/list',{
-        userInfo:req.userInfo,
+
+
+// 显示详情页
+router.get('/detail', (req, res) => {
+    res.render('main/detail', {
+        userInfo: req.userInfo,
     });
 })
+
 
 
 // 导出路由
